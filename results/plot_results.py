3cde40ca2636b2af6eb445bf9e8d45c513009665
Frontier789 <matyas.komaromi@zivid.com>
2024-02-05 13:44:48 +0100

    Added ILP slide

---
+++ results/plot_results.py
@@ -0,0 +1,48 @@
+# %%
+from dataclasses import dataclass
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+
+files = sorted(list(glob("V*.txt")))
+
+
+@dataclass
+class PlotEntry:
+    Ns: list[float]
+    gflops: list[float]
+    name: str
+
+
+entries = []
+
+N_limit = 10000
+
+for file in files:
+    Ns = []
+    gflops = []
+    with open(file) as f:
+        for line in f.readlines():
+            for erase in ["N=", "GFLOPS=", "time=", "\n"]:
+                line = line.replace(erase, "")
+            values = [float(s) for s in line.split(", ")]
+            assert len(values) == 3
+            [N, gflop, time] = values
+            if N < N_limit:
+                Ns.append(N)
+                gflops.append(gflop)
+        entries.append(PlotEntry(Ns, gflops, file.replace(".txt", "")))
+
+
+plot_until = 5
+
+for e in entries[:plot_until]:
+    plt.plot(e.Ns, e.gflops)
+plt.legend([e.name for e in entries[:plot_until]])
+plt.xlabel("N")
+plt.ylabel("GFLOPs")
+
+plt.show()
+
+
+# %%
